82c8f804abf20fd9fa22ea204221f3c0bf059b3d
Asim <asimsalim749@gmail.com>
2023-07-18 17:14:52 +0530

    unwanted file removed

--- server/routes/post.js
+++ server/routes/post.js
@@ -23,62 +23,11 @@ const route=express.Router()
 route.post('/add-post',upload.single('file'), async(req,res)=>{ 
     console.log("title name is",req.body.title)
     
-//   const postData=new postModel({
-//  file:[{
-//     name:'asim',
-    
-//     file:req.file
-//  }
  
-//  ]
-
 
     
-// })
  
-// //     // 
-// //     //
-   
-//         const result=await postData.save()
-//      console.log("post data is",result);
-    
-    //  const doc = await postModel.findOne({ _id: "64b4cd520663d23abed95bce" })
-    //  console.log("doc",doc);
-
-
-     var user_id = '64b55bb5df78daf44c12f8cd';
-//     64b4f18966765db55e8efedd
-//     console.log("file data is",filedata)
-// const upd=await  postModel.findByIdAndUpdate(user_id, { file:req.file }) 
-
-if(user_id!==''){
-//   const upd= await postModel.updateOne({ _id: user_id }, { $push: { name:  req.body.title } })
-//   const upd1= await postModel.updateOne({ _id: user_id }, { $push: { file:req.file } })
-  const upd=await postModel.updateOne({ _id: user_id}, { $push:{ file:  {$each: [{name:req.body.title},req.file] } } })      
  
-  console.log("updated",upd);
-  //console.log("updated",upd1);
-}
-// }else{
-//     const postData=new postModel({
-//  file:[{
-//     name:req.body.title,
-    
-//     file:req.file
-//  }
- 
-//  ]
-
-
-    
-// })
- 
-//     // 
-//     //
-   
-//         const result=await postData.save()
-//      console.log("post data is",result);
-// }
  
         
      try{
